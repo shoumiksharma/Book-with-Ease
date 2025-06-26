a4cc3271e7fe11a1e6fbc3b7a1632c464356c592
Shoumik Sharma <sharmashoumik@gmail.com>
2025-06-27 03:34:51 +0530

    Update navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar(){
                     </div>
                     <div className="buttons text-white w-[40%] h-[100%] flex items-center justify-evenly">
                         <Link to='/' className="border-2 border-white rounded-xl w-[20%] h-[40%] font-Grandstander text-[2vw] text-center">Home</Link>
-                        <Link to='/about' className="border-2 border-white rounded-xl w-[20%] h-[40%] font-Grandstander text-[2vw] text-center">About</Link>
+                        {/* <Link to='/about' className="border-2 border-white rounded-xl w-[20%] h-[40%] font-Grandstander text-[2vw] text-center">About</Link> */}
                         <Link to='/login' className=" rounded-xl w-[20%] h-[40%] font-Grandstander text-[2vw] bg-purple-400 text-center">Login</Link>
                     </div>
                 </div>
